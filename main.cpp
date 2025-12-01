583a5556375c25d3a3b95da090ee2374f8b4d33c
Wonho Lee <wonholee2025@gmail.com>
2025-12-01 14:27:22 -0500

    main file

---
+++ main.cpp
@@ -0,0 +1,104 @@
+/*Wonho Lee
+  Clingan 12:40
+  11/25/25
+*/
+
+#include "FEHLCD.h"
+#include "FEHUtility.h"
+
+void AnimationTraining();
+
+/*
+    SDP Training Minigame #1
+  
+    Make the shapes race across the ​screen.​
+    Change their colors when they reach​ the finish line.​
+*/
+void AnimationTraining() {
+    int x1 = 50, y1 = 50;
+    int x2 = 50, y2 = 100;
+    int r = 10;
+
+    // Draw Racer 1
+    LCD.SetFontColor(RED);
+    LCD.DrawCircle(x1, y1, r);
+    LCD.FillCircle(x1, y1, r);
+
+    // Draw Racer 2
+    LCD.SetFontColor(BLUE);
+    LCD.DrawCircle(x2, y2, r);
+    LCD.FillCircle(x2, y2, r);
+
+    LCD.Update();
+
+    // Make racers move across the screen
+ while (x1-r < 300) {
+
+        // Draw finish line
+        LCD.SetFontColor(WHITE);
+        LCD.DrawLine(300, 10, 300, 220); 
+        
+        
+        // Clear previous racers by drawing black over them
+        LCD.SetFontColor(BLACK);
+        LCD.DrawCircle(x1, y1, r);
+        LCD.FillCircle(x1, y1, r);
+
+        
+        LCD.SetFontColor(BLACK);
+        LCD.DrawCircle(x2, y2, r);
+        LCD.FillCircle(x2, y2, r);
+
+        LCD.Update();
+
+        // Update racer coordinates
+        x1 += 2;
+        x2 += 1;
+        
+
+        // Redraw racers with different colors - don't forget to sleep!
+        LCD.SetFontColor(RED);
+        LCD.DrawCircle(x1, y1, r);
+        LCD.FillCircle(x1, y1, r);
+
+       
+        LCD.SetFontColor(BLUE);
+        LCD.DrawCircle(x2, y2, r);
+        LCD.FillCircle(x2, y2, r);
+
+        LCD.Update();
+
+        if (x1-r >= 300)
+        {
+            LCD.SetFontColor(GREEN);
+            LCD.DrawCircle(x1, y1, r);
+            LCD.FillCircle(x1, y1, r);
+            LCD.Update();
+        }
+
+        if (x2-r >= 300)
+        {
+            LCD.SetFontColor(GREEN);
+            LCD.DrawCircle(x2, y2, r);
+            LCD.FillCircle(x2, y2, r);
+            LCD.Update();
+        }
+
+        Sleep(1);
+        
+    }
+}
+
+int main() {
+    // Clear background
+    LCD.SetBackgroundColor(BLACK);
+    LCD.Clear();
+
+    AnimationTraining();
+
+    while (1) {
+        LCD.Update();
+        // Never end
+    }
+    return 0;
+}
