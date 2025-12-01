690eb3673c8efa2b1c40c8d85d923b414867b9e5
Wonho Lee <wonholee2025@gmail.com>
2025-12-01 14:43:55 -0500

    Draw play button

--- main.cpp
+++ main.cpp
@@ -1,96 +1,27 @@
-/*Wonho Lee
-  Clingan 12:40
-  11/25/25
-*/
-//hello
-//hello
-//goodbye
-
-//hi
 #include "FEHLCD.h"
 #include "FEHUtility.h"
 
-void AnimationTraining();
+void MainMenu();
 
-/*
-    SDP Training Minigame #1
-  
-    Make the shapes race across the ​screen.​
-    Change their colors when they reach​ the finish line.​
-*/
-void AnimationTraining() {
-    int x1 = 50, y1 = 50;
+
+void MainMenu() {
+    int x1 = 130, y1 = 50;
     int x2 = 50, y2 = 100;
-    int r = 10;
+    int x3 = 0, y3 = 0;
+    int x4 = 0, y4 = 0;
+    int width = 60, height = 30;
 
-    // Draw Racer 1
+
+    // Draw Play button
     LCD.SetFontColor(RED);
-    LCD.DrawCircle(x1, y1, r);
-    LCD.FillCircle(x1, y1, r);
+    LCD.DrawRectangle(x1, y1, width, height);
+    LCD.WriteAt("PLAY", x1+5, y1);
 
-    // Draw Racer 2
-    LCD.SetFontColor(BLUE);
-    LCD.DrawCircle(x2, y2, r);
-    LCD.FillCircle(x2, y2, r);
+
 
     LCD.Update();
 
-    // Make racers move across the screen
- while (x1-r < 300) {
-
-        // Draw finish line
-        LCD.SetFontColor(WHITE);
-        LCD.DrawLine(300, 10, 300, 220); 
-        
-        
-        // Clear previous racers by drawing black over them
-        LCD.SetFontColor(BLACK);
-        LCD.DrawCircle(x1, y1, r);
-        LCD.FillCircle(x1, y1, r);
-
-        
-        LCD.SetFontColor(BLACK);
-        LCD.DrawCircle(x2, y2, r);
-        LCD.FillCircle(x2, y2, r);
-
-        LCD.Update();
-
-        // Update racer coordinates
-        x1 += 2;
-        x2 += 1;
-        
-
-        // Redraw racers with different colors - don't forget to sleep!
-        LCD.SetFontColor(RED);
-        LCD.DrawCircle(x1, y1, r);
-        LCD.FillCircle(x1, y1, r);
-
-       
-        LCD.SetFontColor(BLUE);
-        LCD.DrawCircle(x2, y2, r);
-        LCD.FillCircle(x2, y2, r);
-
-        LCD.Update();
-
-        if (x1-r >= 300)
-        {
-            LCD.SetFontColor(GREEN);
-            LCD.DrawCircle(x1, y1, r);
-            LCD.FillCircle(x1, y1, r);
-            LCD.Update();
-        }
-
-        if (x2-r >= 300)
-        {
-            LCD.SetFontColor(GREEN);
-            LCD.DrawCircle(x2, y2, r);
-            LCD.FillCircle(x2, y2, r);
-            LCD.Update();
-        }
-
-        Sleep(1);
-        
-    }
+    
 }
 
 int main() {
@@ -98,7 +29,7 @@ int main() {
     LCD.SetBackgroundColor(BLACK);
     LCD.Clear();
 
-    AnimationTraining();
+    MainMenu();
 
     while (1) {
         LCD.Update();
