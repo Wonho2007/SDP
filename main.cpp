e25f655b2307b48ce31d66053c8953a71f068bc9
Wonho Lee <wonholee2025@gmail.com>
2025-12-03 13:04:43 -0500

    Added levels, combined changes

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include "FEHLCD.h"
 #include "FEHUtility.h"
+#include "FEHRandom.h"
+#include "FEHImages.h"
 
 void MainMenu();
+void StartGame(int);
 void SwitchScreen(int);
 // void Move(int, int);
 
@@ -35,6 +38,88 @@ void SwitchScreen(int);
 
 // }
 
+class question
+{
+public:
+    int level;
+    int num1;
+    int num2;
+    int response;
+    int answer;
+
+
+    question(int = 1, int = 0, int = 0, int = 0, int = 0);
+    void random();
+};
+
+
+//Function to start game
+void StartGame(int level)
+{
+    //Clear screen
+    LCD.SetBackgroundColor(BLACK);
+    LCD.Clear();
+
+    if (level == 1)
+    {
+
+        int questionsAnswered = 0;
+        int x1 = 170, y1 = 20, width = 120, height = 30;
+        int touchx, touchy;
+        bool keepTrackingClicks = true;
+
+
+        // Draw Main Menu button
+        LCD.SetFontColor(RED);
+        LCD.DrawRectangle(x1, y1, width, height);
+        LCD.WriteAt("Main Menu", x1+5, y1+5);
+        LCD.Update();
+
+        //Draw arrow buttons
+        int rectX=175, rectY=170, rectW=105, rectH=30, lineX=227;
+        LCD.SetFontColor(WHITE);
+        LCD.DrawRectangle(rectX, rectY, rectW, rectH);
+        LCD.DrawLine(lineX, 170, lineX, 199);
+        LCD.WriteAt("<--", 178, 175);
+        LCD.WriteAt("-->", 238, 175);
+        LCD.Update();
+
+
+
+
+        question levelOne(1);
+        //Generate a random question
+        levelOne.random();
+        //Import car image and draw
+        FEHImage car("car.png");
+        car.Draw(130, 100);
+        LCD.Update();
+
+
+        while (keepTrackingClicks)
+        {
+            while(!LCD.Touch(&touchx, &touchy)){}
+            while(LCD.Touch(&touchx, &touchy)){}
+
+
+            if (touchx < x1+width && touchx > x1 && touchy > y1 && touchy < y1+height)
+            {
+                MainMenu();
+                keepTrackingClicks = false;
+            } else if (touchx < rectX+(rectW/2) && touchx > rectX && touchy > rectY && touchy < rectY+rectH){
+                //Move car left
+                LCD.WriteAt("left", touchx, touchy);
+                
+            } else if (touchx < rectX+rectW && touchx > rectX+(rectW/2) && touchy > rectY && touchy < rectY+rectH){
+                //Move car right
+                LCD.WriteAt("right", touchx, touchy);
+            }
+        }
+
+
+    }
+}
+
 //Function to switch the screen.
 void SwitchScreen(int screen)
 {
@@ -55,16 +140,26 @@ void SwitchScreen(int screen)
         LCD.WriteAt("Main Menu", x1+5, y1+5);
         LCD.Update();
 
-        //Draw arrow buttons
-        int rectX=175, rectY=170, rectW=105, rectH=30, lineX=227;
-        LCD.SetFontColor(WHITE);
-        LCD.DrawRectangle(rectX, rectY, rectW, rectH);
-        LCD.DrawLine(lineX, 170, lineX, 199);
-        LCD.WriteAt("<--", 178, 175);
-        LCD.WriteAt("-->", 238, 175);
-        LCD.Update();
+        
 
-        //Check if player clicks main menu button
+        // Draw Difficulty buttons
+        int levelButtonx = 110, levelButtony = 60, levelButtonWidth = 100, levelButtonHeight = 30;
+        int levelButtony2 = levelButtony+40;
+        int levelButtony3 = levelButtony2+40; 
+
+        LCD.SetFontScale(1);
+        LCD.SetFontColor(RED);
+        LCD.DrawRectangle(levelButtonx, levelButtony, levelButtonWidth, levelButtonHeight);
+        LCD.WriteAt("LEVEL 1", levelButtonx+5, levelButtony+5);
+
+        LCD.DrawRectangle(levelButtonx, levelButtony2, levelButtonWidth, levelButtonHeight);
+        LCD.WriteAt("LEVEL 2", levelButtonx+5, levelButtony2+5);
+
+        LCD.DrawRectangle(levelButtonx, levelButtony3, levelButtonWidth, levelButtonHeight);
+        LCD.WriteAt("LEVEL 3", levelButtonx+5, levelButtony3+5);
+        
+
+        //Check what button the player clicks
         while (keepTrackingClicks)
         {
             while(!LCD.Touch(&touchx, &touchy)){}
@@ -73,17 +168,25 @@ void SwitchScreen(int screen)
 
             if (touchx < x1+width && touchx > x1 && touchy > y1 && touchy < y1+height)
             {
+
                 MainMenu();
                 keepTrackingClicks = false;
-            } else if (touchx < rectX+(rectW/2) && touchx > rectX && touchy > rectY && touchy < rectY+rectH){
-                //Move car left
-                LCD.WriteAt("left", touchx, touchy);
-                
-            } else if (touchx < rectX+rectW && touchx > rectX+(rectW/2) && touchy > rectY && touchy < rectY+rectH){
-                //Move car right
-                LCD.WriteAt("right", touchx, touchy);
-            }
+
+            } else if (touchx < levelButtonx+levelButtonWidth && touchx > levelButtonx && touchy > levelButtony && touchy < levelButtony+levelButtonHeight)
+            {
+                StartGame(1);
+                keepTrackingClicks = false;
+            } else if (touchx < levelButtonx+levelButtonWidth && touchx > levelButtonx && touchy > levelButtony2 && touchy < levelButtony2+levelButtonHeight)
+            {
+                StartGame(2);
+                keepTrackingClicks = false;
+            } else if (touchx < levelButtonx+levelButtonWidth && touchx > levelButtonx && touchy > levelButtony3 && touchy < levelButtony3+levelButtonHeight)
+            {
+                StartGame(3);
+                keepTrackingClicks = false;
+            } 
         }
+
 
 
     } else if (screen ==2)
@@ -273,4 +376,187 @@ int main() {
         // Never end
     }
     return 0;
+}
+
+question::question(int l, int a, int b, int x, int y)
+{
+
+    level = l;
+    num1 = a;
+    num2 = b;
+    response = x;
+    answer = y;
+
+}
+
+void question::random()
+{
+    int a;
+    int b;
+    int decoy1 = 0;
+    int decoy2 = 0;
+    int randomNumber;
+    char addSub;
+    num1 = 0;
+    num2 = 0;
+
+
+    if (level = 1)
+    {
+
+        //Pick 2 random integers less than 100
+        while (num1 == 0)
+        {
+            a = Random.RandInt()/200;
+
+            if (a < 100)
+            {
+                num1 = a;
+
+            }
+        }
+
+        while (num2==0)
+        {
+            b = Random.RandInt()/200;
+
+            if (b < 100)
+            {
+                num2 = b;
+            }
+            
+        }
+
+        //Pick random addition or subtraction, print question
+        answer = num1+num2;
+
+        if (answer%2 != 0)
+        {
+            addSub = '+';
+            //Write question
+            LCD.SetFontScale(1);
+            LCD.WriteAt(num1, 120, 15);
+            LCD.WriteAt(addSub, 150, 15);
+            LCD.WriteAt(num2, 170, 15);
+
+        } else
+        {
+            addSub = '-';
+            if (num1 >= num2)
+            {
+
+            answer = num1 - num2;
+            //Write question
+            LCD.SetFontScale(1);
+            LCD.WriteAt(num1, 120, 15);
+            LCD.WriteAt(addSub, 150, 15);
+            LCD.WriteAt(num2, 170, 15);
+
+            } else
+            {
+
+                answer = num2 - num1;
+                //Write question
+                LCD.SetFontScale(1);
+                LCD.WriteAt(num2, 120, 15);
+                LCD.WriteAt(addSub, 150, 15);
+                LCD.WriteAt(num1, 170, 15);
+
+            }
+        }
+
+        //Pick random 1-3 gate that the answer falls through.
+        while (decoy2 == 0)
+        {
+            randomNumber = Random.RandInt()/200;
+
+            if (randomNumber > 10 && randomNumber < 15)
+            {
+                //Draw true answer in first box.
+                LCD.DrawRectangle(50, 40, 50, 30);
+                LCD.WriteAt(answer, 50+5, 40+5);
+
+
+                //Draw decoy answer
+                decoy1 = (a + b + randomNumber) * 4.0/5.0 ;
+                LCD.DrawRectangle(130, 40, 50, 30);
+                LCD.WriteAt(decoy1, 130+5, 40+5);
+
+                //Draw decoy answer
+                decoy2 = answer - randomNumber;
+                if (decoy2 > 0)
+                {
+                    LCD.DrawRectangle(210, 40, 50, 30);
+                    LCD.WriteAt(decoy2, 210+5, 40+5);
+                } else
+                {
+                    decoy2 = answer + randomNumber + 5;
+                    LCD.DrawRectangle(210, 40, 50, 30);
+                    LCD.WriteAt(decoy2, 210+5, 40+5);
+                }
+
+            } else if (randomNumber > 15 && randomNumber < 20)
+            {
+                //Draw true answer in second box.
+                LCD.DrawRectangle(130, 40, 50, 30);
+                LCD.WriteAt(answer, 130+5, 40+5);
+
+
+                //Draw decoy answer
+                decoy1 = (a + b + randomNumber) * 4.0/5.0 ;
+                LCD.DrawRectangle(50, 40, 50, 30);
+                LCD.WriteAt(decoy1, 50+5, 40+5);
+
+                //Draw decoy answer
+                decoy2 = answer - randomNumber;
+                if (decoy2 > 0)
+                {
+                    LCD.DrawRectangle(210, 40, 50, 30);
+                    LCD.WriteAt(decoy2, 210+5, 40+5);
+                } else
+                {
+                    decoy2 = answer + randomNumber + 5;
+                    LCD.DrawRectangle(210, 40, 50, 30);
+                    LCD.WriteAt(decoy2, 210+5, 40+5);
+                }
+
+            } else if (randomNumber > 20 && randomNumber < 25)
+            {
+                //Draw true answer in third box.
+                LCD.DrawRectangle(210, 40, 50, 30);
+                LCD.WriteAt(answer, 210+5, 40+5);
+
+
+                //Draw decoy answer
+                decoy1 = (a + b + randomNumber) * 6.0/7.0 ;
+                LCD.DrawRectangle(130, 40, 50, 30);
+                LCD.WriteAt(decoy1, 130+5, 40+5);
+
+                //Draw decoy answer
+                decoy2 = answer - randomNumber;
+                if (decoy2 > 0)
+                {
+                    LCD.DrawRectangle(50, 40, 50, 30);
+                    LCD.WriteAt(decoy2, 50+5, 40+5);
+                } else
+                {
+                    decoy2 = answer + randomNumber + 5;
+                    LCD.DrawRectangle(50, 40, 50, 30);
+                    LCD.WriteAt(decoy2, 50+5, 40+5);
+                }
+
+            }
+
+
+
+        }
+
+        
+
+
+
+    }
+    
+    
+
 }
